e4ba29f66bbeb56b69840d765ae66408d73fa2d7
zhanggg <zhanggg@zhaomei.com>
2024-06-17 16:07:15 +0800

    tmp bash command

--- run_tsl.sh
+++ run_tsl.sh
@@ -1,10 +1,38 @@
 # long-term forecast
-#bash ./scripts/long_term_forecast/ETT_script/TimesNet_ETTh1.sh
-# short-term forecast
+bash ./scripts/long_term_forecast/Exchange_script/Autoformer.sh
+bash ./scripts/long_term_forecast/Exchange_script/Crossformer.sh
+bash ./scripts/long_term_forecast/Exchange_script/FiLM.sh
+bash ./scripts/long_term_forecast/Exchange_script/Koopa.sh
+bash ./scripts/long_term_forecast/Exchange_script/MICN.sh
+bash ./scripts/long_term_forecast/Exchange_script/Mamba.sh
+bash ./scripts/long_term_forecast/Exchange_script/Nonstationary_Transformer.sh
+bash ./scripts/long_term_forecast/Exchange_script/PatchTST.sh
+bash ./scripts/long_term_forecast/Exchange_script/Pyraformer.sh
+bash ./scripts/long_term_forecast/Exchange_script/TimesNet.sh
+bash ./scripts/long_term_forecast/Exchange_script/Transformer.sh
+
+
+#  short-term forecast
+bash ./scripts/short_term_forecast/Autoformer_M4.sh
+bash ./scripts/short_term_forecast/Crossformer_M4.sh
+bash ./scripts/short_term_forecast/DLinear_M4.sh
+bash ./scripts/short_term_forecast/ETSformer_M4.sh
+bash ./scripts/short_term_forecast/FEDformer_M4.sh
+bash ./scripts/short_term_forecast/FiLM_M4.sh
+bash ./scripts/short_term_forecast/Informer_M4.sh
+bash ./scripts/short_term_forecast/LightTS_M4.sh
+bash ./scripts/short_term_forecast/MICN_M4.sh
+bash ./scripts/short_term_forecast/Mamba_M4.sh
+bash ./scripts/short_term_forecast/Nonstationary_Transformer_M4.sh
+bash ./scripts/short_term_forecast/Pyraformer_M4.sh
+bash ./scripts/short_term_forecast/Reformer_M4.sh
+bash ./scripts/short_term_forecast/TSMixer_M4.sh
+bash ./scripts/short_term_forecast/TimeMixer_M4.sh
 bash ./scripts/short_term_forecast/TimesNet_M4.sh
-# imputation
-bash ./scripts/imputation/ETT_script/TimesNet_ETTh1.sh
-# anomaly detection
-bash ./scripts/anomaly_detection/PSM/TimesNet.sh
-# classification
-bash ./scripts/classification/TimesNet.sh
+bash ./scripts/short_term_forecast/Transformer_M4.sh
+bash ./scripts/short_term_forecast/iTransformer_M4.sh
+
+
+
+
+
