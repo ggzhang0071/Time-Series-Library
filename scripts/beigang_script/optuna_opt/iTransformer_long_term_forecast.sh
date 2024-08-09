e0bb95359d33fa6105b470f0fd0c79d4dec12bd8
zhanggg <zhanggg@zhaomei.com>
2024-08-09 13:15:40 +0800

    tmp

--- scripts/beigang_script/optuna_opt/iTransformer_long_term_forecast.sh
+++ scripts/beigang_script/optuna_opt/iTransformer_long_term_forecast.sh
@@ -1,12 +1,12 @@
 export CUDA_VISIBLE_DEVICES=0
 
+target="4500K1.0S" 
 
-root_path='/git/datasets/beigang_data/'
+root_path="/git/datasets/beigang_data/${target}"
 #data_path='all_variables_for_mine_price_4500K1.0S.csv'
 data_path="runmin_an_factors_4500K1.0S.csv"
 #data_path="early_variables_for_mine_price.csv"
 
-target="4500K1.0S" 
 enc_in_choice=78
 data_path="runmin_an_factors_${target}.csv"
 
@@ -19,7 +19,7 @@ config_path="./scripts/beigang_script/optuna_opt/param_config_${target_name}.jso
 
 
 # run_optuna.py  run.py  
-python  -m pdb  /git/Time-Series-Library/run_optuna.py \
+python    /git/Time-Series-Library/run_optuna.py \
   --task_name  $target_name \
   --is_training 1 \
   --root_path $root_path \
